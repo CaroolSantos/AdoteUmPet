fc9fac1c1eb44ffd01291ccce529b22f9ee3ed52
CaroolSantos <a.caroolinasantos@gmail.com>
2016-10-30 19:42:34 -0300

    Atualizando layout do botão. Tela de login e tela de registro do usuário.

--- app/src/main/java/ufrpe/carolina/adoteumpet/activity/RegisterActivity.java
+++ app/src/main/java/ufrpe/carolina/adoteumpet/activity/RegisterActivity.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
@@ -31,6 +32,9 @@ public class RegisterActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.actionBar_title);
 
         btnRegistrar = (Button) findViewById(R.id.btn_registrar);
         btnRegistrar.setOnClickListener(this);
