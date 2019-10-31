d0a9fffe66479ea2399e4715b1fb4a8cf346dcf1
Antonio Correia <antonio.alves.correia@gmail.com>
2019-10-31 17:12:05 -0300

    creating city, pet, shelter and state entities

---
+++ AdoteUmPetBlazor/AdoteUmPet.Model/City.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdoteUmPet.Model
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int StateId { get; set; }
+        public State State { get; set; }
+    }
+}
---
+++ AdoteUmPetBlazor/AdoteUmPet.Model/Specie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdoteUmPet.Model
+{
+    public class Specie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Pet> Pets { get; set; }
+    }
+}
